22fb650018fce797d27721b74157df765a1f20f8
Subhasis Mohapatra <subhasismohapatra3@gmail.com>
2023-06-03 23:49:26 +0530

    added src folder

---
+++ angularapp/src/app/components/adminside/addinstitute/addinstitute.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addinstitute',
+  templateUrl: './addinstitute.component.html',
+  styleUrls: ['./addinstitute.component.css']
+})
+export class AddinstituteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
