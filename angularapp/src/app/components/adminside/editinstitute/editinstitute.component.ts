f672a32769abf375e7575db9ac14e14fbdd40728
ARULSARAVANAN1 <saravansnp@gmail.com>
2023-06-07 15:57:26 +0000

    commit2

---
+++ angularapp/src/app/components/adminside/editinstitute/editinstitute.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editinstitute',
+  templateUrl: './editinstitute.component.html',
+  styleUrls: ['./editinstitute.component.css']
+})
+export class EditinstituteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
