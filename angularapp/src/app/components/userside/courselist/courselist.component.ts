ddae5376e8e515a1c339cddeefb83ceef9b2678b
uec19238 <uec19238@rmd.ac.in>
2023-06-14 06:13:57 +0000

    courselist by teja

--- angularapp/src/app/components/userside/courselist/courselist.component.ts
+++ angularapp/src/app/components/userside/courselist/courselist.component.ts
@@ -5,11 +5,18 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './courselist.component.html',
   styleUrls: ['./courselist.component.css']
 })
-export class CourselistComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class CourselistComponent {
+  users: any[] = [];
+  items: any[] = [];
+  constructor(){
+    this.users = [
+      { Coursename : 'VLSI', Courseduration : '10hr', CourseAvailabletimings : '20days', NumberofStudents : '50', CourseDescription : 'YYY' },
+      
+    ];
+    this.items = [
+      { Coursename : 'CA', Courseduration : '10hr', CourseAvailabletimings : '20days', NumberofStudents : '50', CourseDescription : 'YYY' },
+    ];
   }
+  
 
 }
