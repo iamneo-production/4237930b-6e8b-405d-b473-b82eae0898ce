ea1f469f3aa8d48a8fb700376f179cf398002159
SPD2001 <sindhupriyadharshinisindhu@gmail.com>
2023-07-04 08:43:40 +0000

    updated  conectivity for login amd signup

--- angularapp/src/app/class/user.ts
+++ angularapp/src/app/class/user.ts
@@ -1,7 +1,7 @@
 export class User {
-    email        !:String;
-    password     ?:String;
-    username     ?:String;
-    mobileNumber ?:String;
-    userRole     ?:String;
+    email        !:string;
+    password     ?:string;
+    username     ?:string;
+    mobileNumber ?:string;
+    userRole     ?:string;
   }
