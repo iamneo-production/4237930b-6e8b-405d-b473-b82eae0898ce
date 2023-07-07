8e0835e95c20f86e9eaaf8ae1b44d68749a70be9
Tanu-2505 <tanu19223.it@rmkec.ac.in>
2023-07-07 11:11:34 +0000

    addCourse-updated

--- angularapp/src/app/components/adminside/addcourse/addcourse.component.ts
+++ angularapp/src/app/components/adminside/addcourse/addcourse.component.ts
@@ -3,6 +3,7 @@ import { Course } from 'src/app/class/Course';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AdminserviceService } from 'src/app/service/adminservice/adminservice.service';
 import { ToastrService } from 'ngx-toastr';
+import { FormControl,FormGroup,Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-addcourse',
@@ -15,15 +16,47 @@ export class AddcourseComponent implements OnInit {
 
   course : Course = new Course();
   instituteId !:number;
+  submitted = false;
 
   ngOnInit(): void {
 
     this.instituteId = this.route.snapshot.params['instituteId'];
     console.log(this.instituteId);
   }
+  addcourseForm= new FormGroup({
+    courseName: new FormControl('',[Validators.required]),
+    courseDuration: new FormControl('',[Validators.required]),
+    courseTiming:new FormControl('',[Validators.required]),
+    enrolledStudents:new FormControl('',[Validators.required]),
+    courseDescription:new FormControl('',[Validators.required]),
+})
+
+get courseName()
+  {
+    return this.addcourseForm.get('courseName')
+  }
+  get courseDuration()
+  {
+    return this.addcourseForm.get('courseDuration')
+  }
+  get courseTiming()
+  {
+    return this.addcourseForm.get('courseTiming')
+  }
+  get enrolledStudents()
+  {
+    return this.addcourseForm.get('enrolledStudents')
+  }
+  get courseDescription()
+  {
+    return this.addcourseForm.get('courseDescription')
+  }
 
   onSubmit()
   {
+    this.submitted=true;
+    this.course = this.addcourseForm.value;
+    //this.course.instituteId = this.instituteId;
     console.log(this.course);
     this.addCourse();
   }
