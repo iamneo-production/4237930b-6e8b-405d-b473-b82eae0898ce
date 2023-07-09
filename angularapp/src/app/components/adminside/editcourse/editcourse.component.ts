79d37cf64e98892d20b3928cb8fdbc1251aec2d9
Tanu-2505 <tanu19223.it@rmkec.ac.in>
2023-07-09 11:14:22 +0000

    editCourse updated

--- angularapp/src/app/components/adminside/editcourse/editcourse.component.ts
+++ angularapp/src/app/components/adminside/editcourse/editcourse.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Course } from 'src/app/class/Course';
 import { AdminserviceService } from 'src/app/service/adminservice/adminservice.service';
 import { ToastrService } from 'ngx-toastr';
+import { FormControl,FormGroup,Validators} from '@angular/forms';
+
 @Component({
   
   selector: 'app-editcourse',
@@ -12,8 +14,11 @@ import { ToastrService } from 'ngx-toastr';
 export class EditcourseComponent implements OnInit {
 
   constructor(private router:Router,private adminservice:AdminserviceService,private route:ActivatedRoute,private toastr :ToastrService) { }
+  
   courseId !:number;
   course : Course = new Course();
+  submitted=false;
+
   ngOnInit(): void {
     this.courseId = this.route.snapshot.params['courseId'];
     console.log(this.courseId);
@@ -25,9 +30,40 @@ export class EditcourseComponent implements OnInit {
       });
  
   }
+  editcourseForm= new FormGroup({
+    courseName: new FormControl('',[Validators.required]),
+    courseDuration: new FormControl('',[Validators.required]),
+    courseTiming:new FormControl('',[Validators.required]),
+    enrolledStudents:new FormControl('',[Validators.required]),
+    courseDescription:new FormControl('',[Validators.required]),
+})
+
+get courseName()
+  {
+    return this.editcourseForm.get('courseName')
+  }
+  get courseDuration()
+  {
+    return this.editcourseForm.get('courseDuration')
+  }
+  get courseTiming()
+  {
+    return this.editcourseForm.get('courseTiming')
+  }
+  get enrolledStudents()
+  {
+    return this.editcourseForm.get('enrolledStudents')
+  }
+  get courseDescription()
+  {
+    return this.editcourseForm.get('courseDescription')
+  }
+  
+  
   
 
   onSubmit() {
+    this.submitted = true;
      // display form values on success
     this.adminservice.editCourse(this.courseId,this.course).subscribe({
       next:()=>console.log('updating'),
